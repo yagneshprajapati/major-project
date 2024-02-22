0c86ba06834da317d618d2a25acd933874adc312
sohamdorge45@gmail.com <sohamdorge45@gmail.com>
2024-02-22 20:32:50 +0530

    Auto commit - Changes made

--- Admin/login.php
+++ Admin/login.php
@@ -1,5 +1,5 @@
 <?php
-include 'connection.php';
+include '/wamp64/www/shopflix/Admin/connection.php';
 session_start();
 
 if(isset($_POST['login'])) {
@@ -31,38 +31,102 @@ if(isset($_POST['login'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Admin/style/login.css">
+    <title>Shopflix Login |</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
+
+        body {
+            font-family: 'Poppins', sans-serif;
+            background: #ececec;
+        }
+
+        .box-area {
+            width: 930px;
+        }
+
+        #right-box {
+            padding: 40px 30px 40px 40px;
+        }
+
+        ::placeholder {
+            font-size: 16px;
+        }
+
+        .rounded-4 {
+            border-radius: 20px;
+        }
+
+        .rounded-5 {
+            border-radius: 30px;
+        }
+
+        @media only screen and (max-width: 768px) {
+            .box-area {
+                margin: 0 10px;
+            }
+
+            .left-box {
+                height: 100px;
+                overflow: hidden;
+            }
+
+            #right-box {
+                padding: 20px;
+            }
+        }
+    </style>
 </head>
+
 <body>
 
-<div class="container">
-    <div class="row justify-content-center mt-5">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    Login
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box left-box1" style="background: #480938;">
+                <div class="featured-image mb-3">
+                    <img src="../img/login.png" class="img-fluid" style="width: 250px;">
                 </div>
-                <div class="card-body">
+                <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Be Verified</p>
+                <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Join experienced Designers on this platform.</small>
+            </div>
+            <div class="col-md-6 right-box" id="right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4">
+                        <h2>Hello,Admin</h2>
+                        <p>We are happy to have you back.</p>
+                    </div>
                     <form action="" method="POST">
-                        <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Username" name="username" required>
                         </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password" required>
+                        <div class="input-group mb-1">
+                            <input type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password" name="password" required>
                         </div>
-                        <button type="submit" name="login" class="form-control btn btn-primary">Login</button>
-                    </form>
+                        <div class="input-group mb-5 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="formCheck">
+                                <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
+                            </div>
+                            <div class="forgot">
+                                <small><a href="#">Forgot Password?</a></small>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button ype="submit" name="login" class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                        </div>
+                        </form>  
                 </div>
             </div>
         </div>
     </div>
-</div>
-
 </body>
+
 </html>
