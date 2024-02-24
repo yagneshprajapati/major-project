a6830e783ee8f7177b7eac173b96dc1537f53bf4
yagnesh <zasss1812@gmail.com>
2024-02-24 15:21:57 +0530

    Auto commit - Changes made

---
+++ air/aboutUs.php
@@ -0,0 +1,64 @@
+<?php include('includes/header.php'); ?>
+
+  <main>
+
+
+  <div class="container">
+        <div class="about-us">
+            <h1>About Us</h1>
+            <p>Welcome to EasyFly - Your Trusted Travel Partner</p>
+            <p>At EasyFly, we're passionate about connecting you to the world. With our commitment to excellence and a customer-centric approach, we have become a preferred choice for travelers seeking seamless and affordable airline solutions.</p>
+
+            <h2>Who We Are</h2>
+            <p>EasyFly is a leading airline service provider, specializing in offering a wide range of options from various airline companies. Our dedicated team of travel experts works tirelessly to provide you with the best travel experience possible.</p>
+
+            <h2>Our Mission</h2>
+            <p>To make air travel accessible, convenient, and enjoyable for everyone.</p>
+
+            <h2>Our Vision</h2>
+            <p>To become the go-to platform for hassle-free and cost-effective air travel.</p>
+
+            <h2>Why Choose EasyFly?</h2>
+            <ul>
+                <li>Extensive Network</li>
+                <li>Competitive Pricing</li>
+                <li>Customer-Centric Approach</li>
+                <li>24/7 Support</li>
+            </ul>
+
+            <h2>Our Team</h2>
+            <p>Our success is driven by a team of aviation enthusiasts who are committed to innovation and excellence.</p>
+
+            <h3>Get in Touch</h3>
+            
+          <section id = "targeting-contact">
+          <address>
+                EasyFly Headquarters<br>
+                123 EasyFly Avenue,<br>
+                Cityville, State, ZIP Code<br>
+                Phone: +1-123-456-7890<br>
+                Email: info@easyfly.com
+            </address>
+          </section>
+
+            <div class="social-media">
+                <a href="#"><img src="images/facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
+                <a href="#"><img src="images/linkedin.png" alt="LinkedIn"></a>
+            </div>
+        </div>
+
+        <div class="developers">
+            <h4>Company Owners</h4>
+            <a href= "https://www.linkedin.com/in/miskat141/" target = __blank> <img src="images/mishkat.jpg" alt="Developer 1"></a>
+            <a href = "https://www.linkedin.com/in/shakib-hossian-9ba1a628b/" target = __blank> <img src="images/shakib.jpg" alt="Developer 2"> </a>
+        </div>
+    </div>
+
+
+
+
+  </main>
+
+<?php include('includes/footer.php'); ?>
